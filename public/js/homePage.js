a1fdfb014bd0fbc5751d7489f5153a55f48b0ffd
zhenyu0519 <zhenyu051988@gmail.com>
2016-10-02 05:04:27 -0700

    change the get() path in homePage.js file

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -1,6 +1,7 @@
 var app = angular.module('slideShowApp', []);
 app.controller('slideShowContr', function ($scope, $http) {
-	$http.get("images").success(function (response) {
+	//I think there is problem in get() path is not correct?
+	$http.get("../../controllers//images").success(function (response) {
 		$scope.Images = response.images;
 		//test if the images can be accessed
 		console.log("hello");
