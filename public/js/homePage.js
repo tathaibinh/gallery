6aba7c663dde8f75e2dd7321306796cf0dce429f
zhenyu0519 <zhenyu051988@gmail.com>
2016-10-04 11:27:49 -0700

    change the size of picture to fix the carousel

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -1,6 +1,6 @@
 var app = angular.module('slideShowApp', []);
 app.controller('slideShowContr', function ($scope, $http) {
 	$http.get("images").success(function (response) {
-		$scope.images = response.images;
+		$scope.Images = response.images;
 	});
 });
