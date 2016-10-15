bac40a4474976b604f8cec04a99ee679abbcec36
poanchen <chen.baron@hotmail.com>
2016-10-14 20:18:42 -0700

    add user login on front end for sign up

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -1,5 +1,5 @@
-var app = angular.module('slideShowApp', []);
-app.controller('slideShowContr', function ($scope, $http) {
+var carouselApp = angular.module('carousel-app', []);
+carouselApp.controller('carousel-controller', function ($scope, $http) {
 	$http.get("images").success(function (response) {
 		$scope.Images = response.images;
 	});
---
+++ public/js/user.js
@@ -0,0 +1,8 @@
+var signupApp = angular.module('signup-app', []);
+signupApp.controller('signup-controller', function ($scope, $http) {
+  // $scope.signupFormSubmit = function() {
+  //   console.log("form is about to submit");
+  // };
+  $scope.test = "hello";
+  console.log("test");
+});
