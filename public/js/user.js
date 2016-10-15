b8d0d23d2b2ad2c02b4870b9107744471e07dbe8
poanchen <chen.baron@hotmail.com>
2016-10-14 20:46:29 -0700

    fix the angular not working for signup

--- public/js/user.js
+++ public/js/user.js
@@ -1,8 +1,11 @@
 var signupApp = angular.module('signup-app', []);
 signupApp.controller('signup-controller', function ($scope, $http) {
-  // $scope.signupFormSubmit = function() {
-  //   console.log("form is about to submit");
-  // };
-  $scope.test = "hello";
-  console.log("test");
+  $scope.signupFormSubmit = function() {
+    console.log($scope.email);
+    console.log($scope.firstname);
+    console.log($scope.lastname);
+    console.log($scope.password);
+    console.log($scope.passwordConfirm);
+  };
 });
+angular.bootstrap(document.getElementById("signup-modal"), ['signup-app']);
